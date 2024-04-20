72d2a38433e9e87aa6c94e9367425906995ee992
Xomplyy <sjimnezc@cesde.net>
2024-04-20 12:47:51 -0500

    Formularios
    Cambio de formulario con su boton correspondiente

--- index.php
+++ index.php
@@ -22,17 +22,23 @@
                 <input type="date" name="dateEnd" id="ldateEnd">
             </div>
             <div>
-                <button class="button btn_nav btn_search" type="button" title="Buscar"><img src="img/search.png" alt="icono buscar"></button>
+                <button class="button btn_nav btn_search" type="button" title="Buscar" name="search" id="btn_search"><img src="img/search.png" alt="icono buscar"></button>
             </div>
         </div>
         <div class="btn_header">
-            <button class="button btn_nav" type="button" title="Horario"><img src="img/appointement_management.png" alt="icono horarios"></button>
-            <button class="button btn_nav" type="button" title="Clientes"><img src="img/add__customer.png" alt="icono clientes"></button>
-            <button class="button btn_nav" type="button" title="Citas"><img src="img/appointments.png" alt="icono citas"></button>
+            <button class="button btn_nav" type="button" title="Horario" name="schedules" id="btn_schedules"><img src="img/appointement_management.png" alt="icono horarios"></button>
+            <button class="button btn_nav" type="button" title="Clientes" name="customer" id="btn_customer"><img src="img/add__customer.png" alt="icono clientes" for="btn_customer"></button>
+            <button class="button btn_nav" type="button" title="Citas" name="appointment" id="btn_appointment"><img src="img/appointments.png" alt="icono citas"></button>
         </div>
     </header>
     <main class="main">
         <div class="appointments">
+            <div class="hours hrs_active">
+                <p>09:00</p>
+            </div>
+            <div class="hours hrs_notActive">
+                <p>10:00</p>
+            </div>
         </div>
         <div class="forms">
             <div class="form_appointments form">
@@ -64,9 +70,9 @@
                     </div>
                 </div>
                 <div class="rows buttons">
-                    <button class="button btn"><img src="img/delete_1.png" alt="icono eliminar"></button>
-                    <button class="button btn"><img src="img/save_1.png" alt="icono guardar"></button>
-                    <button class="button btn btn_search"><img src="img/search_1.png" alt="icono buscar"></button>
+                    <button class="button btn" name="delete" id="btn_delete"><img src="img/delete_1.png" alt="icono eliminar"></button>
+                    <button class="button btn" name="save" id="btn_save"><img src="img/save_1.png" alt="icono guardar"></button>
+                    <button class="button btn btn_search" name="searchAppo" id="btn_searchAppo"><img src="img/search_1.png" alt="icono buscar"></button>
                 </div>
             </div>
             <div class="form_customer form">
@@ -104,28 +110,56 @@
                     </div>
                 </div>
                 <div class="rows buttons">
-                    <button class="button btn"><img src="img/delete_1.png" alt="icono eliminar"></button>
-                    <button class="button btn"><img src="img/save_1.png" alt="icono guardar"></button>
-                    <button class="button btn btn_search"><img src="img/search_1.png" alt="icono buscar"></button>
+                    <button class="button btn" name="deleteCust" id="btn_deleteCust"><img src="img/delete_1.png" alt="icono eliminar"></button>
+                    <button class="button btn" name="saveCust" id="btn_saveCust"><img src="img/save_1.png" alt="icono guardar"></button>
+                    <button class="button btn btn_search" name="searchCust" id="btn_searchCust"><img src="img/search_1.png" alt="icono buscar"></button>
                 </div>
             </div>
-            <div class="availability form">
-                <h2>Cliente</h2>
+            <div class="form_schedules form">
+                <h2>Horario</h2>
                 <div class="cont_form">
-                    <div class="row_availability">
+                    <div class="row_schedules">
                         <div>
                             <label for="">Hora</label>
                             <input type="time" name="" id="">
                         </div>
                         <div class="buttons">
-                            <button class="button btn"><img src="img/delete_1.png" alt="icono eliminar"></button>
-                            <button class="button btn"><img src="img/save_1.png" alt="icono guardar"></button>
+                            <button class="button btn" name="deleteSch" id="btn_deleteSch"><img src="img/delete_1.png" alt="icono eliminar"></button>
+                            <button class="button btn" name="saveSch" id="btn_saveSch"><img src="img/save_1.png" alt="icono guardar"></button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </main>
+    <script>
+        document.querySelectorAll('.button').forEach(function(btn){
+            btn.addEventListener('click',function(e){
+                let name = this.name;
+                let schedules = document.querySelector(".form_schedules");
+                let customer = document.querySelector(".form_customer");
+                let appointments = document.querySelector(".form_appointments");
+                switch(name){
+                    case "schedules":
+                        schedules.style.display = 'flex';
+                        customer.style.display = 'none';
+                        appointments.style.display = 'none';
+                        break;
+                    case "customer":
+                        customer.style.display = 'flex';
+                        schedules.style.display = 'none';
+                        appointments.style.display = 'none';
+                        break;
+                    case "appointment":
+                        appointments.style.display = 'flex';
+                        schedules.style.display = 'none';
+                        customer.style.display = 'none';
+                        break;
+                };
+                console.log(name);
+            });
+        });
+    </script>
 </body>
 
 </html>
