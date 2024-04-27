b2d979e7aefb82c85284229c54358202804423a2
Xomplyy <sjimnezc@cesde.net>
2024-04-27 17:55:32 -0500

    Validacion de horas
    Validar que hroas estan disponible y cambiar su estilo con respecto a eso

--- includes/functions.php
+++ includes/functions.php
@@ -1,48 +1,104 @@
-<?php 
-function connection(){
+<?php
+function connection()
+{
     $db = 'database.php';
     return $db;
 };
-function saveSchedules(){
+function getSchedule()
+{
     require connection();
-    $hora = $_POST["hora"];
-    $sql = "INSERT INTO horario (hora) VALUES ('$hora');";
-    $query = mysqli_query($db,$sql);
-    echo $query;
-};
-function deleteSchedules(){
-    require connection();
-    $hora = $_POST["hora"];
-    $sql = "DELETE FROM horario WHERE (hora = '$hora');";
-    $query = mysqli_query($db,$sql);
-    echo $query;
-};
-function printHours(){
-    require connection();
-    $sql = "SELECT * FROM horario;";
-    $query = mysqli_query($db,$sql);
-    return $query;
-};
-////////////////////////////////// FUNCTIONS /////////////////////////////////////////
-if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-    
-    if ($_POST["btn"] == "saveSch"){
-        saveSchedules();
-    };
-    if ($_POST["btn"] == "deleteSch"){
-        deleteSchedules();
-    };
-}
-if($_SERVER['REQUEST_METHOD'] === 'GET'){
     $hours = array();
-    $resultHours = printHours();
-    while($row = mysqli_fetch_assoc($resultHours)){
-        $h = substr($row["hora"],0,5);
-        $hours [] = array (
+    $sql = "SELECT * FROM horario;";
+    $query = mysqli_query($db, $sql);
+    while ($row = mysqli_fetch_assoc($query)) {
+        $h = substr($row["hora"], 0, 5);
+        $hours[] = array(
             'hour' => $h,
         );
     }
-    $json = json_encode($hours);
+    return $hours;
+}
+function saveSchedules()
+{
+    try {
+        require connection();
+        $hora = $_POST["hora"];
+        $sql = "INSERT INTO horario (hora) VALUES ('$hora');";
+        $query = mysqli_query($db, $sql);
+        echo $query;
+    } catch (\Throwable $th) {
+        //throw $th;
+        var_dump($th);
+    }
+};
+function deleteSchedules()
+{
+    try {
+        require connection();
+        $hora = $_POST["hora"];
+        $sql = "DELETE FROM horario WHERE (hora = '$hora');";
+        $query = mysqli_query($db, $sql);
+        echo $query;
+    } catch (\Throwable $th) {
+        // throw $th;
+        var_dump($th);
+    }
+};
+function getHours($date)
+{
+    try {
+        require connection();
+        $sql = "SELECT hora FROM citas WHERE fecha = '$date'";
+        $query = mysqli_query($db, $sql);
+        if ($query->num_rows > 0){
+            while ($row = mysqli_fetch_assoc($query)) {
+                $rowH = substr($row["hora"], 0, 5);
+                $hours[] = array(
+                    "hour" => $rowH
+                );
+            };
+            $hoursSch = getSchedule();
+            foreach ($hours as $key => $h) {
+                foreach ($hoursSch as $key => $hSch) {
+                    if($h == $hSch){
+                        $time []= $h;
+                    }
+                }
+            }
+        }else {
+            $time = [
+                0
+            ];
+        }
+        return $time;
+    } catch (\Throwable $th) {
+        var_dump($th);
+    }
+}
+////////////////////////////////// FUNCTIONS /////////////////////////////////////////
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    if ($_POST["btn"] == "saveSch") {
+        saveSchedules();
+    };
+    if ($_POST["btn"] == "deleteSch") {
+        deleteSchedules();
+    };
+    if($_POST["searchDate"] == "1"){
+        $date = $_POST["date"];
+        $hrjs = [
+            'noActive' => getHours($date)
+        ];
+        $hrjson = json_encode($hrjs);
+        echo $hrjson;
+    }
+}
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+    $js = [
+        'Schedule' => getSchedule(),
+    ];
+    $json = json_encode($js);
     echo $json;
 }
 
